1b9c7b6b29cb0805abfba1d5bf7caccb11492172
kaitlynmcgoldrick <mcgoldrick.kaitlyn@gmail.com>
2017-10-31 03:41:31 +0000

    Filtered LS

---
+++ filtered-ls/program.js
@@ -0,0 +1,13 @@
+const fs = require('fs');
+const path = require('path');
+
+var directory = process.argv[2];
+var extension = '.'+process.argv[3] ;
+
+fs.readdir(directory, function(err, list){
+    for(var i=0;i<list.length;i++){
+        if(path.extname(list[i])==extension){
+            console.log(list[i]);
+        }
+    }
+});
